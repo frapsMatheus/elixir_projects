3c0b1b2225a88bf6573255df774c4a7fd8282c60
Matheus Rosendo Pedreira <matrpedreira@gmail.com>
2024-06-11 22:43:58 -0300

    Finish Identicon project

--- identicon/mix.exs
+++ identicon/mix.exs
@@ -6,6 +6,7 @@ defmodule Identicon.MixProject do
       app: :identicon,
       version: "0.1.0",
       elixir: "~> 1.16",
+
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,6 +22,7 @@ defmodule Identicon.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:egd, github: "erlang/egd"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
